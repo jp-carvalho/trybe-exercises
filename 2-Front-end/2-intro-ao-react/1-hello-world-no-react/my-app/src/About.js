f311bf2e661ac41a97923a694877ca320b06caed
joao-paulo-carvalho <desenhojp@gmail.com>
2023-04-06 19:50:46 -0300

    exercicios do dia

---
+++ 2-Front-end/2-intro-ao-react/1-hello-world-no-react/my-app/src/About.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <>
+        <h1>JP Carvalho</h1>
+        <p>Olá, refazendo exercicios</p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+          <li>Teste1</li>
+          <li>Teste2</li>
+          <li>Teste3</li>
+        </ul>
+      </>
+    );
+  }
+}
+
+export default About;
