02f9ca556159016785e35c244eeaa9994498cb4d
joao-paulo-carvalho <desenhojp@gmail.com>
2023-06-17 11:36:20 -0300

    exercise bonus backend/4.2

--- 3-Back-end/4-introducao-ao-desenvolvimento-web-com-nodejs/2-nodejs-api-rest-com-express/exercises/src/app.js
+++ 3-Back-end/4-introducao-ao-desenvolvimento-web-com-nodejs/2-nodejs-api-rest-com-express/exercises/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { read } = require('fs');
 const fs = require('fs').promises;
 const path = require('path');
 
@@ -36,6 +37,22 @@ app.get('/movies', async (req, res) => {
   }
 });
 
+// exercicio 10 - Bônus
+app.get('/movies/search', (req, res) => {
+  try {
+    const { q } = req.query;
+    const movies = await readFile();
+
+    if (q) {
+      const filteredMovies = movies.filter((element) => element.movie.includes(q));
+      return res.status(200).json(filteredMovies);
+    }
+    res.status(200).end()
+  } catch (err) {
+    res.status(500).send({ message: err.message })
+  }
+})
+
 //exercicio 07
 app.post('/movies', async (req, res) => {
   try {
@@ -48,6 +65,37 @@ app.post('/movies', async (req, res) => {
     };
     const allMovies = JSON.stringify([...movies, newMovie]);
     res.status(200).json(newMovie);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+})
+
+//exercicio 08
+app.put('/movies', async (req, res) => {
+  try {
+    //id desconstruido da requisição (parametros)
+    const { id } = req.params;
+    //movie e price desconstruidos do corpo da req
+    const { movie, price } = req.body;
+    const movies = await readFile();
+    const index = movies.findIndex((element) => element.id === Number(id));
+    movies[index] = { id: Number(id), movie, price };
+    const updatedMovies = JSON.stringify(movies, null, 2);
+    await fd.writeFile(moviesPath, updatedMovies);
+    res.status(200).json(movies[index]);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+app.delete('/movies/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const movies = await readFile();
+    const filteredMovies = movies.filter((movie) => movie.id !== Number(id));
+    const updatedMovies = JSON.stringify(filteredMovies, null, 2);
+    await fs.writeFile(moviesPath, updatedMovies);
+    res.status(204).end();
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
