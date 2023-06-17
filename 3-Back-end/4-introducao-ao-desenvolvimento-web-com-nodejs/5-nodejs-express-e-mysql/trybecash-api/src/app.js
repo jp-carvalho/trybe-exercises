ebec6fcd53ba63a91cad7177cd5c6bbf20fa29a3
joao-paulo-carvalho <desenhojp@gmail.com>
2023-06-17 09:47:44 -0300

    wip: backend/4.5

--- 3-Back-end/4-introducao-ao-desenvolvimento-web-com-nodejs/5-nodejs-express-e-mysql/trybecash-api/src/app.js
+++ 3-Back-end/4-introducao-ao-desenvolvimento-web-com-nodejs/5-nodejs-express-e-mysql/trybecash-api/src/app.js
@@ -3,5 +3,8 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+// app.get('/people', (req, res) => {
+//   res.status(200).json('oi')
+// })
 
 module.exports = app
