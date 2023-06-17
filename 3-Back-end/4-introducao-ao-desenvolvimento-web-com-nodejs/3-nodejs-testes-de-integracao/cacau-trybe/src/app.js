bf0a70d9a84527393975bccac255b3fee905877b
joao-paulo-carvalho <desenhojp@gmail.com>
2023-06-17 13:13:12 -0300

    feat: backend/4.3 bônus exercises

--- 3-Back-end/4-introducao-ao-desenvolvimento-web-com-nodejs/3-nodejs-testes-de-integracao/cacau-trybe/src/app.js
+++ 3-Back-end/4-introducao-ao-desenvolvimento-web-com-nodejs/3-nodejs-testes-de-integracao/cacau-trybe/src/app.js
@@ -4,9 +4,21 @@ const cacauTrybe = require('./cacauTrybe');
 const app = express();
 app.use(express.json());
 
+app.get('/chocolates/search', async (req, res) => {
+  const { name } = req.query;
+  const chocolates = await cacauTrybe.findChocolateByName(name);
+  res.status(chocolates.length === 0 ? 404 : 200)
+    .json(chocolates);
+});
+
 app.get('/chocolates', async (req, res) => {
   const chocolates = await cacauTrybe.getAllChocolates();
   res.status(200).json({ chocolates });
+});
+
+app.get('/chocolates/total', async (req, res) => {
+  const chocolates = await cacauTrybe.getAllChocolates();
+  res.status(200).json({ totalChocolates: chocolates.length });
 });
 
 app.get('/chocolates/:id', async (req, res) => {
@@ -21,5 +33,14 @@ app.get('/chocolates/brand/:brandId', async (req, res) => {
   const chocolates = await cacauTrybe.getChocolatesByBrand(Number(brandId));
   res.status(200).json({ chocolates });
 });
+
+app.put('/chocolates/:id', async (req, res) => {
+  const { id } = req.params;
+  const { name, brandId } = req.body;
+  const updatedChocolate = await cacauTrybe.updateChocolate(Number(id), { name, brandId });
+
+  if (updatedChocolate) return res.status(200).json({ chocolate: updatedChocolate });
+  res.status(404).json({ message: 'chocolate not found' })
+})
 
 module.exports = app;
