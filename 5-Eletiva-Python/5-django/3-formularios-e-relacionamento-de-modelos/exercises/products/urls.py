bde547682e1b27ba78bc9909e7ccf3bc4d2c675d
jp-carvalho <desenhojp@gmail.com>
2024-01-30 00:02:21 -0300

    feat: python/5.3 exercises

---
+++ 5-Eletiva-Python/5-django/3-formularios-e-relacionamento-de-modelos/exercises/products/urls.py
@@ -0,0 +1,12 @@
+# products/urls.py
+from django.urls import path
+from products.views import index, product, seller, buyer, order
+
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("product", product, name="product"),
+    path("seller", seller, name="seller"),
+    path("buyer", buyer, name="buyer"),
+    path("order", order, name="order"),
+]
